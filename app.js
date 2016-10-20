b077cb6bfc8a47ecf1a9f6e47c2e837699a35ff3
KRISHNADAS V <krishna.vadakkath@adesa.com>
2016-10-20 12:46:16 +0530

    Sudoku puzzle

---
+++ app.js
@@ -0,0 +1,57 @@
+var app = angular.module('app', []);
+app.controller('ctrl', ['$scope', function ($scope) {
+
+    $scope.numbers = [
+        [
+       [
+           [null,5,null],
+           [null,7,null],
+           [null,null,null]
+       ],
+         [
+           [null,9,null],
+           [null,4,null],
+           [2,8,null]
+       ],
+         [
+           [null,null,1],
+           [null,null,null],
+           [9,null,null]
+       ],
+        ],
+           [
+       [
+           [null,null,null],
+           [null,null,3],
+           [null,null,null]
+       ],
+         [
+           [null,null,null],
+           [null,null,5],
+           [null,null,null]
+       ],
+         [
+           [null,null,null],
+           [null,null,6],
+           [null,8,null]
+       ],
+        ],
+           [
+       [
+           [null,null,null],
+           [null,null,2],
+           [null,null,8]
+       ],
+         [
+           [null,null,7],
+           [null,null,null],
+           [null,null,null]
+       ],
+         [
+           [5,null,null],
+           [null,null,null],
+           [null,4,null]
+       ],
+        ]
+    ];
+}]);
